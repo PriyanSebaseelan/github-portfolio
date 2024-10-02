9b5ee497982cbb8164b793540f8ac984622dd23f
PriyanSebaseelan <priyanps2007@gmail.com>
2024-10-01 20:05:19 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const backToTop = document.querySelector('.back-to-top');
+const observerTarget = document.querySelector("header");
+
+const observer = new IntersectionObserver((enteries, observer) => {
+    enteries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            backToTop.classList.add("shown");
+        } else {
+            backToTop.classList.remove("shown");
+        }
+    });
+});
+
+observer.observe(observerTarget);
